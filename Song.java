b97d3141113a15370e0177034822d3bcb736a79a
Natnael <natnaelebuy@gmail.com>
2021-11-05 20:36:44 -0700

    update song

---
+++ Song.java
@@ -0,0 +1,40 @@
+
+
+public class Song {
+
+    public String getTitle() {
+        return null;
+    }
+
+    public String getArtist() {
+        return null;
+    }
+
+    public String getGenre() {
+        return null;
+    }
+
+    public String getYear() {
+        return null;
+    }
+
+    public void setArtist(String string) {
+    }
+
+    public void setTitle(String string) {
+    }
+
+    public void setYear(String string) {
+    }
+
+    public void setGenre(String string) {
+    }
+
+    public void setFilePath(String string) {
+    }
+
+    public String getFilePath() {
+        return null;
+    }
+
+}
--- SpotifyLikeApp.java
+++ SpotifyLikeApp.java
@@ -1,149 +1,169 @@
-import java.io.File; 
-import java.io.IOException; 
-import java.util.Scanner; 
-  
-import javax.sound.sampled.AudioInputStream; 
-import javax.sound.sampled.AudioSystem; 
-import javax.sound.sampled.Clip; 
-import javax.sound.sampled.LineUnavailableException; 
-import javax.sound.sampled.UnsupportedAudioFileException;
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.SourceDataLine;
-import javax.sound.sampled.DataLine.Info;
-import javax.swing.JButton;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
-import static javax.sound.sampled.AudioSystem.getAudioInputStream;
-import static javax.sound.sampled.AudioFormat.Encoding.PCM_SIGNED;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 
-/*
-    To compile: javac SpotifyLikeApp.java
-    To run: java SpotifyLikeApp
- */
-
-// declares a class for the app
 public class SpotifyLikeApp {
 
     // global variables for the app
     String status;
     Long position;
     static Clip audioClip;
-    static String song = "./Mysong/NoFriends.wav";
-    static Boolean Playing = false;
-    //private static Clip clip;
-   // private static Object name;
-    //private static Object artist;
-    //private static Object image;
+static HashMap<String, Song> song = new HashMap<>();
+private static HashMap<String, Song> songs;
+private static boolean Playing;
 
-    // "main" makes this class a java app that can be executed
-    public static void main(final String[] args) {
 
+    
+// "main" make this class a java app that can be executed
+public static void main(String[] args) {
+    Song s = new Song();
+            s.setArtist("DJ Khaled ft. Nicki Minaj, Chris Brown, August Alsina, Jeremih, Future & Rick Ross");
+            s.setTitle("Do You Mind");
+            s.setYear("2016");
+            s.setGenre("POP");
+            s.setFilePath("./Song/DJ Khaled- Do You Mind.wav");
+
+            s = new Song();
+            s.setArtist("DJ Khaled ft. Chris Brown, August Alsina,Jermih, future");
+            s.setTitle("Hold you down");
+            s.setYear("2015");
+            s.setGenre("POP");
+            s.setFilePath("./Song/DJ Khaled-Hold you down.wav");
+
+            s = new Song();
+            s.setArtist("Drake ft. Lil Baby");
+            s.setTitle("Girls Want Girls");
+            s.setYear("2021");
+            s.setGenre("Hip-Hop");
+            s.setFilePath("./Song/Drake - Girls Want Girls.wav");
+
+            s = new Song();
+            s.setArtist("Ed - sheeran shape of you");
+            s.setTitle("shape of you");
+            s.setYear("2017");
+            s.setGenre("Pop");
+            s.setFilePath("./Song/Ed -sheeran shape of you.wav");
         // create a scanner for user input
         Scanner input = new Scanner(System.in);
-        String userInput = "";
 
-        while (!userInput.equals("q")) {
+        String userInput = "";
+        while (!userInput.equals("q")){
 
             menu();
 
+        
             // get input
             userInput = input.nextLine();
-            // accept upper or lower case commands
+
+            // accept upper or lower case commands 
             userInput.toLowerCase();
-            // do something
+
+            // do something 
             handleMenu(userInput);
+            
 
         }
 
-        // close the scanner
+        // close the scanner 
         input.close();
+    
+}
 
-    }
+private static void menu() {
 
-    /*
-     * displays the menu for the app
-     */
-    public static void menu() {
-
-        System.out.println("---- SpotifyLikeApp ----");
+    System.out.println("---- SpotifyLikeApp ----");
         System.out.println("[H]ome");
         System.out.println("[S]earch by title");
         System.out.println("[L]ibrary");
         System.out.println("[P]lay");
         System.out.println("[Q]uit");
-        
+        System.out.println("");
 
         System.out.println("");
         System.out.print("Enter q to quit:");
+}
 
+private static void handleMenu(String userInput) {
+    switch(userInput){
+        case "h":
+             System.out.println("-->Home<--");
+             break;
+        case "s":
+            System.out.println("-->Search by title<--");
+            Scanner input = new Scanner(System.in);
+            System.out.println("Please enter the title:");
+            String title = input.nextLine();
+            Song song = songs.get(title);
+        
+
+            if (song !=null)
+            {
+                System.out.println("Your current selection is now playing");
+                play((String) song.getFilePath());
+
+            }else {
+                System.out.println("sorry, please search again.");
+
+                break;
+
+            }
+        case "l":
+            System.out.println("-->Library<--");
+            for(Map.Entry<String, Song> s : songs.entrySet()){
+                System.out.println("Title: " + s.getValue().getTitle());
+                System.out.println("Artist: " + s.getValue().getArtist());
+                System.out.println("Gemre: " + s.getValue().getGenre());
+                System.out.println("Year: " + s.getValue().getYear());
+                System.out.println("");
+            }
+            break;
+        case "p":
+            System.out.println("-->Play<--");
+            break;
+        case "q":
+            System.out.println("-->Quit<--");
+            break;
+        default:
+            break;
     }
 
-    /*
-     * handles the user input for the app
-     */
-    public static void handleMenu(String userInput) {
+}
 
-        switch(userInput) {
+/*
+* plays an audio file
+*/
 
-            case "h":
-                System.out.println("-->Home<--");
-                break;
+private static void play(String filePath) {
+    // open the audio file
+    File file = new File(filePath);
+    try {
+         if (Playing == true)
+         {
+             audioClip.stop();
+             Playing = false;
+         }
 
-            case "s":
-                System.out.println("-->Search by title<--");
-                Search();
-                break;
-
-            case "l":
-                System.out.println("-->Library<--");
-                break;
-                
-            case "p":
-                System.out.println("-->Play<--");
-                play(song);
-                break;
-
-            case "q":
-                System.out.println("-->Quit<--");
-                break;
-            
-
-            default:
-                break;
-        }
-
+        // create clip 
+        audioClip = AudioSystem.getClip();
+        // get input stream
+        final AudioInputStream in = getAudionInputStream(file);
+        audioClip.open(in);
+        audioClip.setMicrosecondPosition(0);
+        audioClip.loop(Clip.LOOP_CONTINUOUSLY);
+        Playing = true;
+    } catch(Exception e) {
+        e.printStackTrace(); 
+    
     }
-
-    /*
-     * plays an audio file
-     */
-    public static void play(String song) {
-        // open the audio file
-        File file = new File(song);
-        try {
-             if (Playing == true)
-             {
-                 audioClip.stop();
-                 Playing = false;
-             }
-
-            // create clip 
-            audioClip = AudioSystem.getClip();
-            // get input stream
-            final AudioInputStream in = getAudioInputStream(file);
-            audioClip.open(in);
-            audioClip.setMicrosecondPosition(0);
-            audioClip.loop(Clip.LOOP_CONTINUOUSLY);
-            Playing = true;
-        } catch(Exception e) {
-            e.printStackTrace(); 
-        
-        }
-        
-        
-            
-        
-    }
-    public static void Rewind() 
+    
+    
+}
+public static void Rewind() 
     {
         Scanner input = new Scanner(System.in);
         audioClip.stop();
@@ -156,32 +176,13 @@ public class SpotifyLikeApp {
         audioClip.loop(Clip.LOOP_CONTINUOUSLY);
         System.out.println(audioClip.getMicrosecondPosition());
     }
+    
 
-    public static void Search() {
-        String[] Songlist;
-        Songlist = new String[5];
-        Songlist[0] = "./Mysong/NoFriends.wav";
-        Songlist[1] = "./Mysong/Vacaciones.wav";
-        Songlist[2] = "./Mysong/Bisou_-_04_-_Journey_of_King.wav";
-        Songlist[3] = "./Mysong/Stop Living A Lie.wav";
-        Songlist[4] = "./Mysong/Drake - Girls Want Girls ft. Lil Baby.wav";
+private static AudioInputStream getAudionInputStream(File file) {
+    return null;
+}
 
-        for(int k = 0; k < Songlist.length; k++)
-        {
-            int a = k+1;
-            System.out.println(a + Songlist[k]);
-        }
-        
-        System.out.println("Please choose a song");
-        Scanner Selection = new Scanner(System.in);
-        int Choice;
-        Choice = Selection.nextInt() - 1;
-        song = Songlist[Choice];
-        
-    }
-
-
+    
 
 
 }
-
